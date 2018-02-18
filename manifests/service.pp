84d3c34f29bb91ce5c1795885c6ca89e809587f3
James Eckersall <james.eckersall@gmail.com>
2018-02-18 13:13:28 +0000

    service

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,16 @@
 class patroni::service {
 
-  service { 'patroni':
+  file { '/lib/systemd/system/patroni.service':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('patroni/patroni.service.erb'),
+  }
+  ~> exec { 'systemctl daemon-reload':
+    refreshonly => true,
+  }
+  -> service { 'patroni':
     ensure => $patroni::service_ensure,
     enable => $patroni::service_enable,
   }

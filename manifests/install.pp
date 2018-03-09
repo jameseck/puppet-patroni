279dc63257f544a62c91a7aca58ae09825e21093
James Eckersall <james.eckersall@gmail.com>
2018-03-09 13:59:34 +0000

    fix pip install command

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,12 +5,27 @@ class patroni::install {
   # install python pip and install patroni python modules
   include python
 
+  case $patroni::config_datastore {
+    'consul': {
+      $patroni_backend_egg = 'python-consul'
+    }
+    'etc': {
+      $patroni_backend_egg = 'python-etcd'
+    }
+    default: {
+      fail("Unsupported Patroni datastore ${patroni::config_datastore}")
+    }
+  }
+
   python::pip { 'psycopg2-binary':
     path => "${patroni::postgres_bindir}:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin",
   }
-  -> python::pip { "patroni[${patroni::config_datastore}]":
-    pkgname => 'patroni',
+  -> python::pip { $patroni_backend_egg:
+    path    => "${patroni::postgres_bindir}:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin",
+  }
+  -> python::pip { 'patroni':
     path    => "${patroni::postgres_bindir}:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin",
   }
 
 }
+

9a34ef2e80c86bff8da2e1793ae0c7134bd2c0e3
James Eckersall <james.eckersall@gmail.com>
2018-03-09 13:28:17 +0000

    fix pip install command

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,8 +5,10 @@ class patroni::install {
   # install python pip and install patroni python modules
   include python
 
-  python::pip { 'psycopg2-binary': }
-  python::pip { 'patroni':
+  python::pip { 'psycopg2-binary':
+    environment => 'PATH=/usr/pgsql-9.6/bin:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin',
+  }
+  -> python::pip { 'patroni':
     install_args => "--install-option=\"${patroni::config_datastore}\"",
   }
 

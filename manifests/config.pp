4443917f1e61b1afc03cdcc927c9a49ab0696b0b
James Eckersall <james.eckersall@gmail.com>
2018-03-09 14:10:32 +0000

    add patronictl support

--- manifests/config.pp
+++ manifests/config.pp
@@ -23,4 +23,23 @@ class patroni::config {
     content => inline_template('<%= @_config_hash.to_yaml %>')
   }
 
+  file { '/root/.config':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0700',
+  }
+  file { '/root/.config/patroni':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0700',
+  }
+  file { '/root/.config/patroni/patronictl.yaml':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => "{dcs_api: '${patroni::config_datastore}://${patroni::config_datastore_host}', namespace: /service/}",
+
 }
